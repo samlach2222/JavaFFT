4649e77c90899b85ed2dfe17b46b3b76b1fc1b73
Samlach22 <samlach22@protonmail.com>
2021-10-11 09:40:19 +0200

    Création du projet JAVA, utilisation de l'IDE IntelliJ IDEA 2021

---
+++ Project/src/com/sl771687/Main.java
@@ -0,0 +1,8 @@
+package com.sl771687;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Projet Fini, enfin il a pas commencé, mais on sais déjà qu'on pourra pas le finir :(");
+    }
+}
