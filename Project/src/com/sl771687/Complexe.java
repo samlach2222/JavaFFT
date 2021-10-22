7dac4aec9246ff0ecbb180b78806573139cac818
samlach22 <samlach22@gmail.com>
2021-10-22 15:30:15 +0200

    Exquise d'un début de programme
    Neon
     — Aujourd’hui à 15:30
    Co-authored-by: Mahtwo <89837262+Mahtwo@users.noreply.github.com>

---
+++ Project/src/com/sl771687/Complexe.java
@@ -0,0 +1,39 @@
+package com.sl771687;
+
+public class Complexe {
+    private double real =0;
+    private double imag =0; //real, imag sont des variables d'instances
+
+    public Complexe() {
+    } // on utilise la valeur par défaut 0 + i0
+
+    public Complexe(double real, double imag) {
+        this.real = real;
+        this.imag = imag;
+    }
+
+    public void conjugate() {imag = -imag;}
+
+    public Complexe add(Complexe c) {
+        //le résultat est aussi un complexe, il faut donc introduire une autre variable de type Complexe
+        Complexe sum = new Complexe();
+        sum.real = real + c.real;
+        sum.imag = imag + c.imag;
+        return sum;
+    }
+
+    public Complexe multiply(Complexe c) {
+        Complexe product = new Complexe();
+        product.real = (real * c.real) - (imag * c.imag);
+        product.imag = (real * c.imag) + (imag * c.imag);
+        return product;
+    }
+
+    public String toString() {
+        // exemple de la méthode prépondérante
+        if (imag>=0)
+            return real + "+i" + Math.abs(imag);
+        else
+            return real + "-i" + Math.abs(imag);
+    }
+}
--- Project/src/com/sl771687/Main.java
+++ Project/src/com/sl771687/Main.java
@@ -1,8 +1,89 @@
 package com.sl771687;
 
+import java.util.ArrayList;
+import java.lang.Math;
+
 public class Main {
 
+     static final int ranMin = -1;
+     static final int ranMax = -1;
+     static final int N = 3; // N = taille du tableau
+
     public static void main(String[] args) {
-        System.out.println("Projet Fini, enfin il a pas commencé, mais on sais déjà qu'on pourra pas le finir :(");
+
+        ArrayList<Complexe> tableau1D = new ArrayList<Complexe>();
+        ArrayList<Complexe> tableauI1D = new ArrayList<Complexe>();
+
+        /*------------------------*/
+        /* Remplissage du tableau */
+        /*------------------------*/
+        /*for(int i = 0; i < N; i++) {
+            tableau1D.add(-1.0); //(Double) Math.random()*(ranMax-ranMin+1)+ranMin
+            tableauI1D.add((Double) Math.random()*(ranMax-ranMin+1)+ranMin);
+        }*/
+
+        tableau1D.add(new Complexe(-1.0,0));
+        tableau1D.add(new Complexe(0.0,0));
+        tableau1D.add(new Complexe(1.0,0));
+
+        /*-------------------------*/
+        /* Affichage des resultats */
+        /*-------------------------*/
+
+        /*System.out.println("Transformée inverse I1D:");
+        for (Complexe c : Transformee1D(tableau1D)) {
+            System.out.println(c.toString());
+        }*/
+        /*System.out.println("Transformée inverse I1D:");
+        /*for (Complexe c : TransformeeInverse1D(tableauI1D)){
+            System.out.println(c.toString());
+        }*/
+    }
+
+    static private ArrayList<Complexe> Transformee1D(ArrayList<Complexe> tableau1D){
+        // Création tableau resultats
+        ArrayList<Complexe> resultat = new ArrayList<Complexe>();
+
+        for(int i = 0; i < N; i++) {
+            // Création de la variable de somme des éléments du tableau
+            Complexe additionLocale = new Complexe(0,0);
+            for(int j = 0; j < N; j++) {
+                double teta = -2.0*Math.PI*i*j/N;
+                Complexe part1 = tableau1D.get(j).multiply(new Complexe(Math.cos(teta),0));
+                Complexe part2 = tableau1D.get(j).multiply(new Complexe(Math.sin(teta),0));
+                Complexe a = new Complexe(part1,part2); // Nombre complexe 3+2
+
+                additionLocale = additionLocale.add(a);
+            }
+            resultat.add(additionLocale);
+            additionLocale = new Complexe(0,0);
+        }
+
+        return resultat;
+    }
+
+    /**
+     * @param tableauI1D
+     * @return Le résultat de la transformation inverse 1D
+     */
+    private static ArrayList<Complexe> TransformeeInverse1D(ArrayList<Double> tableauI1D){
+        ArrayList<Complexe> resultat = new ArrayList<Complexe>();
+        for (int x = 0; x < N; x++){
+            Complexe c = new Complexe(0,0);
+            for (int u = 0; u < N-1; u++){
+                double i_MaisJeSaisPasAQuoiIlCorrespond = 1;
+                double valeurAExponentialise = (2*i_MaisJeSaisPasAQuoiIlCorrespond*Math.PI*u*x) / N;
+                double reelle = tableauI1D.get(u)*Math.cos(valeurAExponentialise);
+                double imaginaire = tableauI1D.get(u)*Math.sin(valeurAExponentialise);
+
+                Complexe complexeAAjouter = new Complexe(reelle, imaginaire);
+
+                c = c.add(complexeAAjouter);
+
+            }
+            resultat.add(c);
+        }
+
+        return resultat;
     }
 }
