9fb953bfe5024c13b3ee5948806f864d41136cbf
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-11-17 21:11:33 +0100

    Basé sur la fonction Transformee1D
    Pour l'instant incorrect : inverser le résultat d'une transformée 1D ne donne pas les valeurs de départs

--- Project/src/com/sl771687/Main.java
+++ Project/src/com/sl771687/Main.java
@@ -18,7 +18,7 @@ public class Main {
         final int N = 5; // N = taille du tableau
 
         ArrayList<Complexe> tableau1D = new ArrayList<Complexe>();
-        ArrayList<Complexe> tableauI1D = new ArrayList<Complexe>();
+        //ArrayList<Complexe> tableauI1D = new ArrayList<Complexe>();
 
         TF1D tf1d = new TF1D();
         TFR1D tfr1d = new TFR1D();
@@ -26,40 +26,41 @@ public class Main {
         /*------------------------*/
         /* Remplissage du tableau */
         /*------------------------*/
-        for(int i = 0; i < N; i++) {
+        /*for(int i = 0; i < N; i++) {
             tableau1D.add(new Complexe(Math.random()*(ranMax-ranMin+1)+ranMin,Math.random()*(ranMax-ranMin+1)+ranMin)); // nonbre complexe aléatoire entre -5 et 5 sur ses deux parties
             //tableauI1D.add((Double) Math.random()*(ranMax-ranMin+1)+ranMin);
-        }
+        }*/
 
         /*------------------------*/
         /*   DEBUG INSERT VALUES  */
         /*------------------------*/
-        /*tableau1D.add(new Complexe(-1.0,0));
+        tableau1D.add(new Complexe(-1.0,0));
         tableau1D.add(new Complexe(0.0,0));
-        tableau1D.add(new Complexe(1.0,0));*/
+        tableau1D.add(new Complexe(1.0,0));
 
         /*-----------------------------------------------*/
         /* Affichage des resultats de TF1D.Transformee1D */
         /*-----------------------------------------------*/
-        /*System.out.println("Transformée 1D:");
-        for (Complexe c : tf1d.Transformee1D(tableau1D)) {
+        System.out.println("Transformée 1D:");
+        ArrayList<Complexe> tableauDontLinverseDoitRevenirSur_tableau1D = tf1d.Transformee1D(tableau1D);
+        for (Complexe c : tableauDontLinverseDoitRevenirSur_tableau1D) {
             System.out.println(c.toString());
-        }*/
+        }
 
         /*------------------------------------------------------*/
         /* Affichage des resultats de TF1D.TransformeeInverse1D */
         /*------------------------------------------------------*/
-        /*System.out.println("Transformée inverse I1D:");
-        for (Complexe c : tf1d.TransformeeInverse1D(tableauI1D)){
+        System.out.println("Transformée inverse I1D:");
+        for (Complexe c : tf1d.TransformeeInverse1D(tableauDontLinverseDoitRevenirSur_tableau1D)){
             System.out.println(c.toString());
-        }*/
+        }
 
         /*------------------------------------------------------*/
         /* Affichage des resultats de TFR1D.TransformeeRapide1D */
         /*------------------------------------------------------*/
-        System.out.println("Transformée rapide I1D:");
+        /*System.out.println("Transformée rapide I1D:");
         for (Complexe c : tfr1d.TransformeeRapide1D(tableauI1D,N)){
             System.out.println(c.toString());
-        }
+        }*/
     }
 }
--- Project/src/com/sl771687/TF1D.java
+++ Project/src/com/sl771687/TF1D.java
@@ -41,22 +41,24 @@ public class TF1D {
     public ArrayList<Complexe> TransformeeInverse1D(ArrayList<Complexe> tableauI1D){
         // Récupération de la taille du tableau
         int N = tableauI1D.size();
-
+        // Création tableau resultats
         ArrayList<Complexe> resultat = new ArrayList<Complexe>();
-        for (int x = 0; x < N; x++){
-            Complexe c = new Complexe(0,0);
-            for (int u = 0; u < N; u++){
-                double teta = (2*Math.PI*u*x)/ N;
-                double reelle = tableauI1D.get(u).getReal()*Math.cos(teta);
-                double imaginaire = tableauI1D.get(u).getImag()*Math.sin(teta);
 
-                Complexe complexeAAjouter = new Complexe(reelle, imaginaire);
-                c = c.add(complexeAAjouter);
+        for(int i = 0; i < N; i++) {
+            // Création de la variable de somme des éléments du tableau
+            Complexe additionLocale = new Complexe(0, 0);
+            for (int j = 0; j < N; j++) {
+                double teta = (2.0 * Math.PI * i * j) / N; // angle teta
+                double realPart = tableauI1D.get(j).getReal() * Math.cos(teta); // partie réelle
+                double imagPart = tableauI1D.get(j).getImag() * Math.sin(teta); // partie imaginaire
+                Complexe a = new Complexe(realPart, imagPart); // création du nombre complexe
 
+                additionLocale = additionLocale.add(a); // addition avec les nombres complexes précédents
             }
-            resultat.add(c);
+            Complexe unDiviseParN = new Complexe((double) 1/N,0);
+            additionLocale.multiply(unDiviseParN);
+            resultat.add(additionLocale);
         }
-
         return resultat;
     }
 }
