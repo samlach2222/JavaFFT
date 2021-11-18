b931c02f0130c7021677396c710897801220dd34
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-11-18 20:29:06 +0100

    Transformée de Fourier Inverse 1D (#2)
    * Basé sur la fonction Transformee1D
    
    Pour l'instant incorrect : inverser le résultat d'une transformée 1D ne donne pas les valeurs de départs
    
    * Corrige transformée inverse 1D
    
    * Ajoute transformée dans la transformée inverse
    
    La transformée inverse n'était pas bonne et ne l'est toujours pas
    
    * Enlève les packages
    
    * Enlève les fichiers non-utilisés du projet
    
    * Ajoute une configuration de run partagé
    
    * Ajoute runConfigurations.xml au gitignore
    
    C'est un plugin non-utilisé qui créé ce fichier par erreur
    
    * Transformée Fourier Inverse 1D pas fonctionnel
    
    * Fonctionne toujours pas
    
    * Transforme la classe TF1D en statique
    
    * Enlève le correcteur orthographique
    
    * TFR1D fonctionnelle (problème dans inverse) + ajout round 1 décimale
    
    * Met à jour la branche
    
    * Corrige toString partie imaginaire
    
    * Non ça fonctionne pas
    
    * Mise a jour de la doc + retrait d'include en trop
    
    * ajout de la documentation JavaDoc
    
    * TransformeeFourierInverse1D fini
    
    Co-authored-by: samlach22 <samlach22@gmail.com>

--- Project/src/TF1D.java
+++ Project/src/TF1D.java
@@ -46,25 +46,28 @@ public final class TF1D {
     /**
      * Méthode permettant de calculer la Transformée de Fourrier 1D Inverse
      * @param tableauI1D tableau 1D de valeurs auquel on veut appliquer la transformée inverse 1D
-     * @return Le résultat de la transformation inverse 1D
+     * @return le résultat de la Transformée de Fourrier Inverse 1D
      */
     public static ArrayList<Complexe> TransformeeInverse1D(ArrayList<Complexe> tableauI1D){
         // Récupération de la taille du tableau
         int N = tableauI1D.size();
-
+        // Création tableau resultat
         ArrayList<Complexe> resultat = new ArrayList<Complexe>();
-        for (int x = 0; x < N; x++){
-            Complexe c = new Complexe(0,0);
-            for (int u = 0; u < N; u++){
-                double teta = (2*Math.PI*u*x)/ N;
-                double reelle = tableauI1D.get(u).getReal()*Math.cos(teta);
-                double imaginaire = tableauI1D.get(u).getImag()*Math.sin(teta);
 
-                Complexe complexeAAjouter = new Complexe(reelle, imaginaire);
-                c = c.add(complexeAAjouter);
+        for(int i = 0; i < N; i++) {
+            // Création de la variable de somme des éléments du tableau
+            Complexe additionLocale = new Complexe(0, 0);
+            for (int j = 0; j < N; j++) {
+                double teta = (2.0 * Math.PI * i * j) / N; // angle teta
+                double realPart = tableauI1D.get(j).getReal() * Math.cos(teta);  //partie réelle
+                double imagPart = tableauI1D.get(j).getReal() * Math.sin(teta);  //partie imaginaire
+                Complexe a = new Complexe(realPart, imagPart); // création du nombre complexe
 
+                additionLocale = additionLocale.add(a); // addition avec les nombres complexes précédents
             }
-            resultat.add(c);
+            additionLocale = additionLocale.multiply(new Complexe((double) 1/N, 0));
+
+            resultat.add(additionLocale);
         }
 
         return resultat;
