d15abf3e4470446d3c75a0dfdd7b133605275535
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-11-18 20:29:06 +0100

    Transformée de Fourier Inverse 1D (#2)
    * Basé sur la fonction Transformee1D
    
    Pour l'instant incorrect : inverser le résultat d'une transformée 1D ne donne pas les valeurs de départs
    
    * Corrige transformée inverse 1D
    
    * Ajoute transformée dans la transformée inverse
    
    La transformée inverse n'était pas bonne et ne l'est toujours pas
    
    * Enlève les packages
    
    * Enlève les fichiers non-utilisés du projet
    
    * Ajoute une configuration de run partagé
    
    * Ajoute runConfigurations.xml au gitignore
    
    C'est un plugin non-utilisé qui créé ce fichier par erreur
    
    * Transformée Fourier Inverse 1D pas fonctionnel
    
    * Fonctionne toujours pas
    
    * Transforme la classe TF1D en statique
    
    * Enlève le correcteur orthographique
    
    * TFR1D fonctionnelle (problème dans inverse) + ajout round 1 décimale
    
    * Met à jour la branche
    
    * Corrige toString partie imaginaire
    
    * Non ça fonctionne pas
    
    * Mise a jour de la doc + retrait d'include en trop
    
    * ajout de la documentation JavaDoc
    
    * TransformeeFourierInverse1D fini

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -23,6 +23,7 @@ public class Main {
         /* Remplissage du tableau */
         /*------------------------*/
         /*for(int i = 0; i < N; i++) {
+			      tableau1D.add(new Complexe(Math.random()*(ranMax-ranMin+1)+ranMin,Math.random()*(ranMax-ranMin+1)+ranMin)); // nonbre complexe aléatoire entre -5 et 5 sur ses deux parties
             tableauI1D.add(new Complexe(Math.random()*(ranMax-ranMin+1)+ranMin,Math.random()*(ranMax-ranMin+1)+ranMin)); // nonbre complexe aléatoire entre -5 et 5 sur ses deux parties
         }*/
 
@@ -48,21 +49,30 @@ public class Main {
         tableauI1D.add(new Complexe(-4.0,0.0));
         tableauI1D.add(new Complexe(0.0,0.0));*/
 
+        /*------------------------------*/
+        /* Affichage du tableau de base */
+        /*------------------------------*/
+        /*System.out.println("Tableau par défaut:");
+        for (Complexe c : tableau1D){
+            System.out.println(c);
+        }*/
+
         /*-----------------------------------------------*/
         /* Affichage des résultats de TF1D.Transformee1D */
         /*-----------------------------------------------*/
         System.out.println("Transformée 1D:");
-        for (Complexe c : TF1D.Transformee1D(tableauI1D)) {
+		    ArrayList<Complexe> tableauDontLinverseDoitRevenirSur_tableau1D = TF1D.Transformee1D(tableau1D);
+        for (Complexe c : tableauDontLinverseDoitRevenirSur_tableau1D) {
             System.out.println(c.toString());
         }
 
         /*------------------------------------------------------*/
         /* Affichage des résultats de TF1D.TransformeeInverse1D */
         /*------------------------------------------------------*/
-        /*System.out.println("Transformée inverse I1D:");
-        for (Complexe c : TF1D.TransformeeInverse1D(tableauI1D)){
+        System.out.println("Transformée inverse I1D:");
+        for (Complexe c : TF1D.TransformeeInverse1D(tableauDontLinverseDoitRevenirSur_tableau1D)){
             System.out.println(c.toString());
-        }*/
+        }
 
         /*------------------------------------------------------*/
         /* Affichage des résultats de TFR1D.TransformeeRapide1D */
