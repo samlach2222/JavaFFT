e459fac913df858d556b950037adcf05e676c62a
Mahtwo <89837262+Mahtwo@users.noreply.github.com>
2021-11-25 17:42:21 +0100

    Ajoute transformée 2D pas rapide

---
+++ FFT_Java/src/TF2D.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+/**
+ * Classe pour la Transformée de Fourier 2D et 2D Inverse
+ * @author Loïs PAZOLA
+ * @author Samuel LACHAUD
+ * @version 1.1
+ */
+public final class TF2D {
+
+    /**
+     * Constructeur de la classe TF2D
+     * @throws Exception si quelqu'un essaye d'utiliser le constructeur de la classe statique
+     */
+    private TF2D() throws Exception {
+        throw new Exception("Cette classe est statique et ne doit pas être initialiser");
+    }
+
+    /**
+     * Méthode permettant de calculer la Transformée de Fourier 2D
+     * @param tableau2D tableau 2D de valeurs auquel on veut appliquer la transformée 2D
+     * @param sensTransformee sens de la transformée (-1 pour transformée normale, 1 pour transformée inverse)
+     * @return le résultat de la Transformée de Fourier 2D
+     * @throws Exception si la taille du tableau n'est pas une puissance de 2
+     */
+    public static ArrayList<ArrayList<Complexe>> Transformee2D(ArrayList<ArrayList<Complexe>> tableau2D, int sensTransformee) throws Exception{
+        // Récupération de la taille du tableau
+        int X = tableau2D.size();
+        int Y = tableau2D.get(0).size();
+
+        if((X & (X - 1)) != 0 || (Y & (Y - 1)) != 0){
+            throw new Exception("Le tableau doit avoir comme taille une puissance de 2 !");
+        }
+        else{
+            // Création du tableau de résultat et initialisation de ses colonnes
+            ArrayList<ArrayList<Complexe>> resultat = new ArrayList<ArrayList<Complexe>>();
+            for (int i = 0; i < X; i++){
+                resultat.add(new ArrayList<Complexe>());
+            }
+
+            for (int i = 0; i < X; i++) {  //i correspond au rang horizontal du prochain résultat
+                for (int j = 0 ; j < Y; j++) {  //j correspond au rang vertical du prochain résultat
+                    // Création de la variable de somme des éléments du tableau
+                    Complexe additionLocale = new Complexe(0, 0);
+                    for (int k = 0; k < X; k++) {  //horizontal
+                        for (int l = 0; l < Y; l++) {  //vertical
+                            double theta = (sensTransformee * 2.0 * Math.PI * i * j * k * l) / (X*Y); // Angle thêta
+                            final Complexe complexeOriginal = tableau2D.get(k).get(l);
+                            double realPart = complexeOriginal.getReal() * Math.cos(theta) + complexeOriginal.getImag() * Math.sin(theta); // Partie réelle
+                            double imagPart = -complexeOriginal.getReal() * Math.sin(theta) + complexeOriginal.getImag() * Math.cos(theta); // Partie imaginaire
+                            Complexe a = new Complexe(realPart, imagPart); // Création du nombre complexe
+
+                            additionLocale = additionLocale.add(a); // Addition avec les nombres complexes précédents
+                        }
+                    }
+                    if(sensTransformee == 1){ // Si sens transformé inverse
+                        additionLocale = additionLocale.multiply(1.0/(X*Y));
+                    }
+                    resultat.get(i).add(additionLocale);
+                }
+            }
+            return resultat;
+        }
+    }
+}
